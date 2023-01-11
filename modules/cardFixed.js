a880da7aeaa7f807e78839026618111a1b80f6c4
Gabriel Vieira <gabriel.vieirasilva0712@gmail.com>
2023-01-11 18:35:56 -0300

    some fixes

--- modules/cardFixed.js
+++ modules/cardFixed.js
@@ -3,11 +3,20 @@ var card = document.querySelector('.card-perfil');
 
 
 window.addEventListener('scroll', function () {
-    if (pageYOffset > 2178) {
-        card.style.position = 'sticky';
-        card.style.top = '100px';
+    var altura = window.screen.height;
+    var largura = window.screen.width;
+    console.log(altura)
+    console.log(largura)
+    if (altura > 600 && largura > 600) {
+        if (pageYOffset > 2178) {
+            card.style.position = 'sticky';
+            card.style.top = '100px';
+        } else {
+            card.style.position = 'static';
+        }
     } else {
         card.style.position = 'static';
     }
+
 
 }); 
